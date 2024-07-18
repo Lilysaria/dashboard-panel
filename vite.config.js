ef1126772de7471b2bea6dc557b3db104ca58ee8
Lilysaria <bloomyday56@gmail.com>
2024-07-17 22:20:29 -0700

    fix(build): ensure Vite outputs to dist directory
    - Explicitly set outDir to 'dist' in vite.config.js
    - Set build script in package.json to use Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
-    }
+    },
+    cors: true
   },
   plugins: [react()],
   build: {
     manifest: true,
+    outDir: 'dist',
     rollupOptions: {
       input: "./src/main.jsx",
     },
