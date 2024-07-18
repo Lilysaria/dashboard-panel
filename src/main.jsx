dacb1afec7e1b83fe09e96b2e78fce17bfd1fd81
Lilysaria <bloomyday56@gmail.com>
2024-07-18 14:37:51 -0700

    fix(build): update for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,17 @@ import 'semantic-ui-css/semantic.min.css'
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
+if (import.meta.env.DEV) {
+  import('http://localhost:5173/@react-refresh').then((RefreshRuntime) => {
+    RefreshRuntime.injectIntoGlobalHook(window);
+    window.$RefreshReg$ = () => {};
+    window.$RefreshSig$ = () => (type) => type;
+    window.__vite_plugin_react_preamble_installed__ = true;
+  });
+
+  import('http://localhost:5173/@vite/client');
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import cssnano from 'cssnano';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
@@ -14,22 +13,13 @@ export default defineConfig({
     manifest: true,
     outDir: 'dist',
     rollupOptions: {
-      input: "./src/main.jsx",
+      input: './index.html',
       output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
       },
     },
     cssCodeSplit: true,
-    css: {
-      postcss: {
-        plugins: [
-          cssnano({
-            preset: 'default',
-          }),
-        ],
-      },
-    },
   },
 });
